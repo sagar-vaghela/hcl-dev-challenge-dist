13aaadce915fdff13d92da410004fe034df46426
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2020-12-07 15:05:38 +0530

    add files for test case

---
+++ site/test/dummy-stomp.js
@@ -0,0 +1,14 @@
+module.exports = (function () {
+    return {
+        client: function(){
+            return {
+                connect: function () {
+                    
+                },
+                debug: function () {
+                    
+                }
+            }
+        }
+    }
+})();
