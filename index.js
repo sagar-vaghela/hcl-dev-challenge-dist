85e76788f77f8a07e19e6427d6de404d2c32ce50
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2020-12-07 15:05:59 +0530

    update description

--- index.js
+++ index.js
@@ -5,32 +5,38 @@
  */
 
 // This is not really required, but means that changes to index.html will cause a reload.
-require('./site/index.html')
+require('./site/index.html');
 // Apply the styles in style.css to the page.
-require('./site/style.css')
+require('./site/style.css');
 
 // if you want to use es6, you can do something like
 //     require('./es6/myEs6code')
 // here to load the myEs6code.js file, and it will be automatically transpiled.
 
+// This will initialize and manage data emitter and html renderer.This will get the data from Data emitter and will parse it to html renderer.
+const TableModule = require('./site/fx_price_table_module');
+
+// Will emit the latest data received. Anyone listening to 'data-receive' event will get the latest data.
+const DataHandler = require('./site/fx_service_data_emitter');
+
+// This will be initialized by table module. It will get data from table module and render results to UI.
+const HTMLHandler = require('./site/fs_price_table_html_renderer');
+
 // Change this to get detailed logging from the stomp library
-global.DEBUG = false
+global.DEBUG = false;
 
-const url = "ws://localhost:8011/stomp"
-const client = Stomp.client(url)
-client.debug = function(msg) {
-  if (global.DEBUG) {
-    console.info(msg)
-  }
+document.addEventListener('DOMContentLoaded', function () {
+    initPage();
+});
+
+function initPage(){
+    var container = document.getElementById('root');
+    var config = {
+        container: container,
+        sortByColumn: 'lastChangeBid'
+    };
+    new TableModule(config, DataHandler, HTMLHandler, window.Sparkline, window.Stomp);
 }
 
-function connectCallback() {
-  document.getElementById('stomp-status').innerHTML = "It has now successfully connected to a stomp server serving price updates for some foreign exchange currency pairs."
-}
 
-client.connect({}, connectCallback, function(error) {
-  alert(error.headers.message)
-})
 
-const exampleSparkline = document.getElementById('example-sparkline')
-Sparkline.draw(exampleSparkline, [1, 2, 3, 6, 8, 20, 2, 2, 4, 2, 3])
